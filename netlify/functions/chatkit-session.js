5dc5983c42e4d3460ab120fed5e4911eac1f3402
Emel Maestro <esm.taxconsultant@kataxpayer.com>
2025-10-13 12:36:18 +0800

    string to object

--- netlify/functions/chatkit-session.js
+++ netlify/functions/chatkit-session.js
@@ -43,7 +43,9 @@ exports.handler = async (event, context) => {
                 "OpenAI-Beta": "chatkit_beta=v1",
             },
             body: JSON.stringify({
-                workflow: WORKFLOW_ID,
+                workflow: {
+                    id: "wf_68e499b75644819092b2e8ed2c3a28ad0d6f86991ff8a181" // The ID string from your Netlify environment variable
+                },
                 user: {
                     id: userData.deviceId || 'anonymous-user', 
                     name: 'Web User' 
